5cc7d7ad653c267347a88dd0df278b9ff3ff2212
uka <sign.up.uka@gmail.com>
2019-06-30 11:42:10 +0900

    こんどこそはろーわーるどできたよー

---
+++ src/main/java/com/example/smpleboard/SmpleBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.smpleboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmpleBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmpleBoardApplication.class, args);
+	}
+
+}
